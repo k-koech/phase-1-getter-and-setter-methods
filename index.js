1e009f8b04b69c5d2bb816e1fbca64f51c7245d2
k-koech <kelvin.koech@student.moringaschool.com>
2022-06-27 12:15:34 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your Circle class here
+class Circle
+{
+    constructor(radius){
+        this.radius =radius
+    }
+    get diameter()
+    {
+        return this.radius*2
+    }
+    get circumference()
+    {
+        return this.diameter*3.142;
+    }
+
+    get area()
+    {
+        let area = this.radius*this.radius*3.142;
+        return parseFloat(area.toFixed(1));
+    }
+
+    // SETS
+    set diameter(diameter)
+    {
+        return this.radius = diameter/2
+    }
+    set circumference(circumference)
+    {
+        return this.radius = circumference/(3.142*2)
+    }
+
+    set area(area)
+    {
+        return this.radius = Math.sqrt(area/3.142);
+    }
+}
